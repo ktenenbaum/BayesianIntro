b66978619e07db4fcec5132c89969bbebee7ed98
ktenenbaum <ktenenbaum@middlebury.edu>
2015-05-13 20:11:45 -0400

    R Data package upload

---
+++ R/BayesianIntro.R
@@ -0,0 +1,15 @@
+#' pitchfx.
+#'
+#' Pitchfx data
+#'
+#' @format A data frame with seven variables: \code{X}, \code{pitcher},
+#'   \code{name}, \code{velocity}, \code{horizMovement}, \code{vertMovement}, and \code{KPCT}
+"pitchfx"
+
+#' OBPData.
+#'
+#' On Base Percentage Data
+#'
+#' @format A data frame with 4 variables: \code{playerID}, \code{Name}, \code{PA} and
+#'   \code{OBP}
+"OBPData"
